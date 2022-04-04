057ef1f7b0a09befb47053563b3c3cd1386d7b34
mayragann <mayragann@gmail.com>
2022-04-04 12:40:47 -0400

    Added Model Classes for Types for Supers and SuperTypes

--- supers/models.py
+++ supers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Supers(models.Model):
+    name = models.CharField(max_length=255)
+    alter_ego = models.CharField(max_length=255)
+    primary_ability = models.CharField(max_length=255)
+    secondary_ablity = models.CharField(max_length=255)
+
+
+
