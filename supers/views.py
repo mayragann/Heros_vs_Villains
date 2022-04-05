84cf17e9a580ebe82ec5ff79ddb58700dee80d2d
mayragann <mayragann@gmail.com>
2022-04-05 14:08:20 -0400

    Added Function to Custom View Function Response and View Qeury Params

--- supers/views.py
+++ supers/views.py
@@ -2,22 +2,53 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+
+from super_types.models import SuperType
 from .serializers import SuperSerializer
 from .models import Super
 
+
+
 @api_view(['GET', 'POST'])
 def supers_list(request):
+    supers_param = request.query_params.get('type')
+    supers = Super.objects.all()
+
+    super_types = SuperType.objects.all()
+    custom_response_dictionary = {}
+
+
+
+    if supers_param:
+        supers = Super.objects.filter(super_type__type=supers_param)
+        serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data)
+    
     
     if request.method == 'GET':
         supers = Super.objects.all()
-        serializer = SuperSerializer(supers, many=True)
-        return Response(serializer.data)
+
+        for super_type in super_types:
+            supers = Super.objects.filter(super_type_id=super_type.id)
+            serializer = SuperSerializer(supers, many=True)
+            custom_response_dictionary[super_type.type] = serializer.data
+        return Response(custom_response_dictionary)
 
     elif request.method == 'POST':
         serializer = SuperSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+# @api_view(['GET'])
+# def supers_list(request):
+#     supers_param = request.query_params.get('super_type')
+#     supers = Super.objects.all()
+#     if supers_param:
+#         supers = supers.filter(super_type__name='super_type')
+#     serializer = SuperSerializer(supers, many=True)
+#     return Response(serializer.data)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def super_detail(request, pk):
